80a738fd9d8a1658c99a29ff6309991693fd3225
zzw <zzwss8969@163.com>
2019-05-23 09:20:37 +0800

    实体类 car 信息更新

--- src/main/java/com/wadn/carssm/pojo/Car.java
+++ src/main/java/com/wadn/carssm/pojo/Car.java
@@ -6,4 +6,20 @@ package com.wadn.carssm.pojo;
 public class Car {
     private String name;
     private String user;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
 }
